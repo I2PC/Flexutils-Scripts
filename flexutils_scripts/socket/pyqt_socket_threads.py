2a34acdd74a0ad6efd57427daf2101c07b47ea87
DavidHerreros <david.herreroscalero@gmail.com>
2024-02-27 12:26:47 +0100

    Fix import

--- flexutils_scripts/socket/pyqt_socket_threads.py
+++ flexutils_scripts/socket/pyqt_socket_threads.py
@@ -31,7 +31,7 @@ from xmipp_metadata.image_handler import ImageHandler
 
 from PyQt5.QtCore import QThread, pyqtSignal
 
-from client import Client
+from flexutils_scripts.socket.client import Client
 
 from flexutils_scripts import getProgram, runProgram
 
