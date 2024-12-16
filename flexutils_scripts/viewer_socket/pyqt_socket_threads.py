dc565fe0cc355c9caad5c2300e1d1bd7ccf8bc60
DavidHerreros <david.herreroscalero@gmail.com>
2024-12-16 11:29:41 +0100

    Annotate space can now work with atomic models

--- flexutils_scripts/viewer_socket/pyqt_socket_threads.py
+++ flexutils_scripts/viewer_socket/pyqt_socket_threads.py
@@ -32,6 +32,9 @@ import time
 
 import numpy as np
 from xmipp_metadata.image_handler import ImageHandler
+from scipy.ndimage import gaussian_filter
+
+from napari_molecule_reader.molecule_reader import read_molecules
 
 from PyQt5.QtCore import QThread, pyqtSignal
 
@@ -74,6 +77,37 @@ class ClientQThread(QThread):
         map = ImageHandler().read(file).getData()
         return np.squeeze(map)
 
+    def readModel(self, file):
+        # Read PDB or MMCIF file
+        _, points = read_molecules(
+            file)  # This returns coordinates and vectors in case we want to have a visualization based on bonds
+        points = points[0][..., 1:]
+
+        # Prepare grid to place coordinates
+        grid = np.zeros((128, 128, 128))
+
+        # Move center of mass to origin, scale, and displace to create the indices
+        points = points - np.mean(points, axis=0)
+        scale_factor = (0.5 * 128) / np.max(np.linalg.norm(points, axis=1))
+        points = scale_factor * points + 0.5 * 128
+
+        # Ensure coordinates are within grid bounds by rounding to the nearest integer
+        rounded_coords = np.rint(points).astype(int)
+
+        # Place the intensities into the grid
+        intensities = np.ones(points.shape[0])
+        for coord, intensity in zip(rounded_coords, intensities):
+            x, y, z = coord
+            # Check bounds to avoid out-of-range errors
+            if 0 <= x < 128 and 0 <= y < 128 and 0 <= z < 128:
+                grid[x, y, z] += intensity  # Accumulate intensity if multiple points map to the same voxel
+
+        # Apply a Gaussian filter to smooth the grid
+        sigma = 1.0  # Standard deviation for Gaussian filter
+        smoothed_grid = gaussian_filter(grid, sigma=sigma)
+
+        return smoothed_grid
+
     def run(self):
         np.savetxt(os.path.join(self.path, "z_server.txt"), self.z)
         while not os.path.isfile(os.path.join(self.path, "z_server.txt")) and not os.access(os.path.join(self.path, "z_server.txt"), os.R_OK):
@@ -96,15 +130,23 @@ class ClientQThread(QThread):
 
         # Emit signals
         if self.z.shape[0] == 1:
-            generated_map = self.readMap(vol_file.format(1))
+            if vol_file.split(".")[-1] == "mrc":
+                generated_map = self.readMap(vol_file.format(1))
+            elif vol_file.split(".")[-1] == "pdb" or vol_file.split(".")[-1] == "cif":
+                generated_map = self.readModel(vol_file.format(1))
             self.volume.emit(generated_map)
             os.remove(vol_file.format(1))
         else:
             for idx in range(self.z.shape[0]):
                 new_path = os.path.join(self.path, self.file_names[idx] + ".mrc")
-                if vol_file.format(idx + 1) != new_path:
-                    ImageHandler().convert(vol_file.format(idx + 1), new_path, overwrite=True)
-                    os.remove(vol_file.format(idx + 1))
+                formatted_vol_file = vol_file.format(idx + 1)
+                if formatted_vol_file.split(".")[-1] == "pdb" or formatted_vol_file.split(".")[-1] == "cif":
+                    generated_map = self.readModel(formatted_vol_file)
+                    ImageHandler().write(generated_map, new_path)
+                    os.remove(formatted_vol_file)
+                elif formatted_vol_file.split(".")[-1] == "mrc" and formatted_vol_file != new_path:
+                    ImageHandler().convert(formatted_vol_file, new_path, overwrite=True)
+                    os.remove(formatted_vol_file)
 
         # Emit signals
         if self.z.shape[0] > 1:
